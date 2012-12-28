9d4c8b4b32f98e0716ab4f7abafa4659da5ac402
Arseniy <fokinarseniy69@gmail.com>
2012-12-29 07:19:22 +0800

    Поправил сбившиеся строки в accountmanager и createtable.

--- createtable.php
+++ createtable.php
@@ -2,15 +2,15 @@
 function createDB(mysqli $db)
 {
 	$sql = 'CREATE TABLE `user` (											'
-			. ' `id`			INT UNSIGNED	NOT NULL 	AUTO_INCREMENT,		'
-					. ' `login`			VARCHAR(32)		NOT NULL	UNIQUE,				'
-							. ' `pass`			VARCHAR(32)		NOT NULL,						'
-									. ' `hash`			VARCHAR(32)		NOT NULL	UNIQUE,				'
-											. ' `regdate`		TIMESTAMP		DEFAULT CURRENT_TIMESTAMP, 		'
-													. ' `lastlogin`		TIMESTAMP		DEFAULT 0,						'
-															. ' PRIMARY KEY (`id`)												'
-																	. ' )																'
-																			. ' ENGINE = InnoDB CHARACTER SET utf8;								';
+		. ' `id`			INT UNSIGNED	NOT NULL 	AUTO_INCREMENT,		'
+		. ' `login`			VARCHAR(32)		NOT NULL	UNIQUE,				'
+		. ' `pass`			VARCHAR(32)		NOT NULL,						'
+		. ' `hash`			VARCHAR(32)		NOT NULL	UNIQUE,				'
+		. ' `regdate`		TIMESTAMP		DEFAULT CURRENT_TIMESTAMP, 		'
+		. ' `lastlogin`		TIMESTAMP		DEFAULT 0,						'
+		. ' PRIMARY KEY (`id`)												'
+		. ' )																'
+		. ' ENGINE = InnoDB CHARACTER SET utf8;								';
 	if ($db->query($sql))
 		echo "<p>Table 'user' created</p>";
 	else
@@ -19,13 +19,13 @@ function createDB(mysqli $db)
 	$sql = 'CREATE TABLE `files` (												'
 			. ' `id`		INT UNSIGNED	NOT NULL 	AUTO_INCREMENT,				' //Введен в целях оптимизации работы БД (оставим на будущее)
 			. '	`path`		VARCHAR(255)	NOT NULL	UNIQUE,						'
-					. '	`user_id`	INT	UNSIGNED	NOT NULL,								'
-							. '	`comment`	TEXT			NOT NULL,								'
-									. '	`date`		TIMESTAMP		NOT NULL	DEFAULT CURRENT_TIMESTAMP,	'
-											. '	PRIMARY KEY (`id`),													'
-													. ' FOREIGN KEY (`user_id`) REFERENCES user(`id`) ON DELETE CASCADE		'
-															. '	)																	'
-																	. ' ENGINE = InnoDB CHARACTER SET utf8;									';
+			. '	`user_id`	INT	UNSIGNED	NOT NULL,								'
+			. '	`comment`	TEXT			NOT NULL,								'
+			. '	`date`		TIMESTAMP		NOT NULL	DEFAULT CURRENT_TIMESTAMP,	'
+			. '	PRIMARY KEY (`id`),													'
+			. ' FOREIGN KEY (`user_id`) REFERENCES user(`id`) ON DELETE CASCADE		'
+			. '	)																	'
+			. ' ENGINE = InnoDB CHARACTER SET utf8;									';
 	if ($db->query($sql))
 		echo "<p>Table 'files' created</p>";
 	else
